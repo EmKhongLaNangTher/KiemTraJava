fa9e595286b4185729522565082086e68c57bd35
EmKhongLaNangTher <148929364+EmKhongLaNangTher@users.noreply.github.com>
2024-06-16 10:21:09 +0700

    final

--- LeTrungTien/src/test/java/com/trungtien/TienApplicationTests.java
+++ LeTrungTien/src/test/java/com/trungtien/TienApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class KhaiApplicationTests {
+class TienApplicationTests {
 
 	@Test
 	void contextLoads() {
